af7a141d77b5c6332d6aef2b5e7dd3e0a8d6c050
Mykhailo Shvorak <mshvorak@gmail.com>
2021-09-23 22:54:39 +0300

    tuned motor pwm to better match speed

--- src/core/scripts/cmd_vel_to_jetson.py
+++ src/core/scripts/cmd_vel_to_jetson.py
@@ -3,11 +3,23 @@ import rospy
 from geometry_msgs.msg import Twist
 import RPi.GPIO as GPIO
 
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
 def callback(msg):
     if msg.linear.x > 0.2:
         print ("forward")
-        motor_left.ChangeDutyCycle(100 * msg.linear.x)
-        motor_right.ChangeDutyCycle(100 * msg.linear.x)
+	cycle = translate(msg.linear.x, 0.2, 1, 80, 100)
+        motor_left.ChangeDutyCycle(cycle)
+        motor_right.ChangeDutyCycle(cycle)
         GPIO.output(12, GPIO.HIGH)
     else:
         motor_left.ChangeDutyCycle(0)
